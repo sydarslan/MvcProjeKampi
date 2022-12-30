1caea4ebf904da466de80e2cae13ff8bbd308b0c
Şeyda Arslan <seydarslan098@gmail.com>
2022-12-30 19:13:08 +0300

    MvcProjeKampi
    Asp.net mvc Proje kampı

---
+++ EntityLayer/Heading.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class Heading
+    {
+        public int HeadingId { get; set; }
+        public string HeadingName { get; set;}
+        public DateTime HeadingDate { get; set;}
+
+        public int CategoryId { get; set;}
+        public virtual Category Category { get; set; }
+        public int WriterId { get; set; }
+        public virtual Writer Writer { get; set; }
+        public ICollection<Content> Contents { get; set; }
+
+       
+
+    }
+}
---
+++ EntityLayer/Writer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class Writer
+    {
+        public int WriterId { get; set; }
+        public string WriterName { get; set; }
+        public string WriterSurname { get; set; }
+        public string WriterImage { get; set; }
+        public string WriterEmail { get; set; }
+        public string WriterPassword{ get; set; }
+
+        public ICollection<Heading> Headings { get; set; }
+        public ICollection<Content> Contents { get; set; }
+    }
+}
