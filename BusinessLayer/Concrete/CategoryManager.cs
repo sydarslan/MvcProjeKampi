58604d4c299dc3eb2925c04db3009b70ed8ad29f
Şeyda Arslan <seydarslan098@gmail.com>
2023-01-02 12:41:35 +0300

    BusinessLayer İşlemleri yapıldı- sertifika görev 1 tamamlandı

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,34 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Önce repository tanıtılmalı 
+// Sonra repository içinde tanımlanan metotlar yazılmalı
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+        public List<Category> GetAllBL()  // - R
+        {
+            return repo.List();
+             
+        }
+        public void CategoryAddBL(Category p) // -C 
+        {
+            if(p.CategoryName=="" || p.CategoryName.Length<=3 || p.CategoryDescription=="" || p.CategoryName.Length>=51)
+            {
+                //hata mesajı fırlatır
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+        }
+    }
+}
